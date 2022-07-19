5bb89d3e3ec31f08a7cde83efd414595adb6c12f
namratapandule <pandulenamrata1997@gmail.com>
2022-07-19 10:09:39 +0530

    node project setup.

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require("body-parser");
+process.env.NODE_ENV = !process.env.NODE_ENV
+? "development"
+: process.env.NODE_ENV;
+
+//initialise express router
+var router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Error handling
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.bangarbus.com");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+  );
+  next();
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen( port, (req, res) => {
+  console.log(`Server is running on ${port} port.`);
+});
+
+
+
