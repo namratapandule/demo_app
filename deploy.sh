ab407bc1c637b7822795e269b552bc056910aa47
namratapandule <pandulenamrata1997@gmail.com>
2022-07-22 14:04:07 +0530

    setup

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 version=`cat .version`
 export VERSION=$version
-docker rm nodeapi && \
+con_id=`docker ps -aqf "name=nodeapi"`
+if [ "$con_id" != 0 ]
+then
+
+docker stop "$con_id"
+docker rm nodeapi
+
 docker run -p 3000:5000 -d --name nodeapi nodeapi/node:$VERSION
